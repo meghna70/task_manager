e04b86546c7ca08d4132b3d1e7ee4f90754b336f
meghna70 <meghnabal70@gmail.com>
2023-06-10 17:43:36 +0530

    Create SideBar.jsx

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>
+        <div className='sideBar-title'>
+              Title 
+        </div>
+        <div className='sideBar-content'>
+         Content 
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
